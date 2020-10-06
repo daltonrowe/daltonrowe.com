87a1ad87c8522c7e5f3291f1f4ba2226e2082665
daltonrowe <cdaltonrowe@gmail.com>
2020-10-05 20:04:38 -0500

    themes update

--- js/theme1.js
+++ js/theme1.js
@@ -1,1 +1,11 @@
-console.log("its theme 1");
+// no styles
+
+// const allStyles = document.querySelectorAll('link[rel="stylesheet"]');
+
+// console.log("no styles", allStyles);
+
+// for (let i = 0; i < allStyles.length; i++) {
+//   const style = allStyles[i];
+//   console.log("remove:", style);
+//   style.remove();
+// }

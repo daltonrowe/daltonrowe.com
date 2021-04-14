ed3c53a771aac585ecd295f2f0071b581a8b7401
daltonrowe <cdaltonrowe@gmail.com>
2021-04-13 19:51:49 -0500

    switch back to png, add resume

--- misc/convert-webp.sh
+++ misc/convert-webp.sh
@@ -1,17 +1,17 @@
-# https://www.digitalocean.com/community/tutorials/how-to-create-and-serve-webp-images-to-speed-up-your-website
+# https://www.digitalocean.com/community/tutorials/how-to-create-and-serve-png-images-to-speed-up-your-website
 
 # converting JPEG images
 find $1 -type f -and \( -iname "*.jpg" -o -iname "*.jpeg" \) \
 -exec bash -c '
-webp_path=$(sed 's/\.[^.]*$/.webp/' <<< "$0");
-if [ ! -f "$webp_path" ]; then
-  cwebp -quiet -q 90 "$0" -o "$webp_path";
+png_path=$(sed 's/\.[^.]*$/.png/' <<< "$0");
+if [ ! -f "$png_path" ]; then
+  cpng -quiet -q 90 "$0" -o "$png_path";
 fi;' {} \;
 
 # converting PNG images
-find $1 -type f -and -iname "*.webp" \
+find $1 -type f -and -iname "*.png" \
 -exec bash -c '
-webp_path=$(sed 's/\.[^.]*$/.webp/' <<< "$0");
-if [ ! -f "$webp_path" ]; then
-  cwebp -quiet -near_lossless 75 "$0" -o "$webp_path";
+png_path=$(sed 's/\.[^.]*$/.png/' <<< "$0");
+if [ ! -f "$png_path" ]; then
+  cpng -quiet -near_lossless 75 "$0" -o "$png_path";
 fi;' {} \;

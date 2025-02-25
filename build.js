a8a80fbfe3f0816e35a65ce5e339c51f3d4b9494
Dalton <cdaltonrowe@gmail.com>
2025-02-25 10:43:13 +0100

    use templating system

--- build.js
+++ build.js
@@ -2,16 +2,20 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 
 function template(template, contents, defaults) {
-  const [metaStr, content] = contents.split("%%%");
-  const meta = { ...JSON.parse(metaStr), ...defaults };
-
   let markup = template;
 
-  for (const key in meta) {
-    markup = markup.replaceAll(`{{${key}}}`, meta[key]);
-  }
+  if (contents.indexOf("%%%") !== -1) {
+    const [metaStr, content] = contents.split("%%%");
+    const meta = { ...defaults, ...JSON.parse(metaStr) };
 
-  markup = markup.replace("{{content}}", content);
+    for (const key in meta) {
+      markup = markup.replaceAll(`{{${key}}}`, meta[key]);
+    }
+
+    markup = markup.replace("{{content}}", content);
+  } else {
+    markup = markup.replace("{{content}}", contents);
+  }
 
   return markup;
 }
@@ -21,14 +25,13 @@ function template(template, contents, defaults) {
 const distPath = path.join(import.meta.dirname, "dist");
 const distExists = fs.existsSync(distPath);
 
-if (distExists) fs.rmdirSync(distPath, { recursive: true });
-fs.mkdirSync(distPath)
+if (distExists) fs.rmSync(distPath, { recursive: true });
+fs.mkdirSync(distPath);
 
 // generate articles
 
 const articlesPath = path.join(import.meta.dirname, "articles");
-const articles = fs
-  .readdirSync(articlesPath);
+const articles = fs.readdirSync(articlesPath);
 
 const articlesTemplatePath = path.join(
   import.meta.dirname,
@@ -40,29 +43,27 @@ const articleTemplate = fs.readFileSync(articlesTemplatePath, {
 });
 
 for (const article of articles) {
-  const articlePath = path.join(articlesPath, article)
+  const articlePath = path.join(articlesPath, article);
   const articleContent = fs.readFileSync(articlePath, { encoding: "utf-8" });
 
   const markup = template(articleTemplate, articleContent, {
     title: "",
     headline: "",
-    subtitle: ""
+    subtitle: "",
   });
 
-  const outPath = path.join(distPath, article)
+  const outPath = path.join(distPath, article);
   fs.writeFileSync(outPath, markup, { encoding: "utf-8" });
 }
 
 // copy static assets
 
-fs.cpSync(
-  path.join(import.meta.dirname, "favicon.svg"),
-  path.join(import.meta.dirname, "dist"),
-);
-fs.cpSync(
-  path.join(import.meta.dirname, "index.html"),
-  path.join(import.meta.dirname, "dist"),
-);
+for (const rootFile of ["favicon.svg", "index.html", "resume.html"]) {
+  fs.cpSync(
+    path.join(import.meta.dirname, rootFile),
+    path.join(import.meta.dirname, "dist", rootFile),
+  );
+}
 
 for (const cpPath of ["css", "img", "js"]) {
   fs.cpSync(

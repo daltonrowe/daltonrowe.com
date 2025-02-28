d350e13f1d75ff985b66d4124e653d5ba5db3403
Dalton <cdaltonrowe@gmail.com>
2025-02-28 08:44:09 +0100

    lint

--- scripts/link.js
+++ scripts/link.js
@@ -2,24 +2,25 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 
 const linkPath = path.join(import.meta.dirname, "..", "links");
-const links = fs.readdirSync(linkPath).map(f => f.split('.')[0]);
+const links = fs.readdirSync(linkPath).map((f) => f.split(".")[0]);
 
 const today = new Date();
-const filename = today.toISOString().split('T')[0];
+const filename = today.toISOString().split("T")[0];
 
-let offset = '';
+let offset = "";
 
-const dupes = links.filter(f => f.includes(filename))
+const dupes = links.filter((f) => f.includes(filename));
 
 if (dupes.length) {
-  const last = dupes.at(-1)
-  offset = `_${parseInt(last.split('_')[1]) + 1}`
+  const last = dupes.at(-1);
+  offset = `_${Number.parseInt(last.split("_")[1]) + 1}`;
 }
 
 const linkContent = {
-  "url": "",
-  "description": ""
-}
+  url: "",
+  description: "",
+  quote: "",
+};
 
-const newLink = path.join(linkPath, `${filename + offset}.json`)
-fs.writeFileSync(newLink, JSON.stringify(linkContent, null, 2))
+const newLink = path.join(linkPath, `${filename + offset}.json`);
+fs.writeFileSync(newLink, `${JSON.stringify(linkContent, null, 2)}\n`);

c2ec57cafef32e7c8b70fea8c8251bcf91e81c59
Dalton <cdaltonrowe@gmail.com>
2025-04-29 08:38:16 +0200

    new thought script

---
+++ scripts/new.js
@@ -0,0 +1,55 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+const [, , type] = process.argv;
+
+let content;
+let distDir
+let distFile
+
+if (type === 'link') {
+  const [, , , url, description, quote] = process.argv;
+
+  const data = {
+    url: url ?? "",
+    description: description ?? "",
+    quote: quote ?? "",
+  }
+
+  content = JSON.stringify(data, null, 2)
+
+  distDir = 'links'
+  distFile = 'json'
+}
+
+if (type === 'thought') {
+  const [, , , thought] = process.argv;
+
+  content = `<p>${thought}</p>`;
+
+  distDir = 'thoughts'
+  distFile = 'html'
+}
+
+const distPath = path.join(import.meta.dirname, "..", distDir);
+const previous = fs.readdirSync(distPath).map((f) => f.split(".")[0]);
+
+const today = new Date();
+const filename = today.toISOString().split("T")[0];
+
+let offset = "";
+
+const dupes = previous.filter((f) => f.includes(filename));
+
+if (dupes.length) {
+  const last = dupes.at(-1);
+
+  if (last.includes("_")) {
+    offset = `_${Number.parseInt(last.split("_")[1]) + 1}`;
+  } else {
+    offset = "_1";
+  }
+}
+
+const next = path.join(distPath, `${filename + offset}.${distFile}`);
+fs.writeFileSync(next, `${content}\n`);
